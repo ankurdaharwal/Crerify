43aa0280791cce7280cf573fc9bcef5e6b4d5a1d
Ankur Daharwal <ankur.daharwal@gmail.com>
2017-10-01 05:28:19 +0530

    Crerify v1.0

--- CompanyRegistration.go
+++ CompanyRegistration.go
@@ -1,26 +1,21 @@
 package main
 
 import (
-	
+	"encoding/json"
 	"errors"
 	"fmt"
-	"encoding/json"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
+
 type Company struct {
-
 }
-
 
 type COMPANYJSON struct {
-
-CompanyId string `json: "CompanyId"` 
-CompanyName string `json: "CompanyName"`
-CompanyStatus string `json: "CompanyStatus"`
-CompanyRegTime string `json: "CompanyRegTime"`
-
+	CompanyId      string `json: "CompanyId"`
+	CompanyName    string `json: "CompanyName"`
+	CompanyStatus  string `json: "CompanyStatus"`
+	CompanyRegTime string `json: "CompanyRegTime"`
 }
-
 
 func (t *Company) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	// Check if table already exists
@@ -30,46 +25,41 @@ func (t *Company) Init(stub shim.ChaincodeStubInterface, function string, args [
 		return nil, nil
 	}
 
-
 	// Create BOL Table
 	err = stub.CreateTable("CompanyRegistration", []*shim.ColumnDefinition{
 		&shim.ColumnDefinition{Name: "Type", Type: shim.ColumnDefinition_STRING, Key: true},
 		&shim.ColumnDefinition{Name: "CompanyId", Type: shim.ColumnDefinition_STRING, Key: true},
 		&shim.ColumnDefinition{Name: "CompanyName", Type: shim.ColumnDefinition_STRING, Key: false},
 		&shim.ColumnDefinition{Name: "CompanyStatus", Type: shim.ColumnDefinition_STRING, Key: false},
-		&shim.ColumnDefinition{Name: "CompanyRegTime", Type: shim.ColumnDefinition_STRING, Key: false}
+		&shim.ColumnDefinition{Name: "CompanyRegTime", Type: shim.ColumnDefinition_STRING, Key: false},
 	})
 	if err != nil {
 		return nil, errors.New("Failed creating Company Registration Table.")
 	}
 
-
 	return nil, nil
 
+}
+
+func (t *Company) SubmitDoc(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	if len(args) != 4 {
+		return nil, fmt.Errorf("Incorrect number of arguments. Expecting 4. Got: %d.", len(args))
 	}
 
+	CompanyId := args[0]
+	CompanyName := args[1]
+	CompanyStatus := args[2]
+	CompanyRegTime := args[3]
 
-	func (t *Company) SubmitDoc(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-
-		
-		if len(args) != 4 {
-			return nil, fmt.Errorf("Incorrect number of arguments. Expecting 4. Got: %d.", len(args))
-		}
-
-		CompanyId := args[0]
-		CompanyName := args[1]
-		CompanyStatus := args[2]
-		CompanyRegTime := args[3]
-
-		// Insert a row
+	// Insert a row
 	ok, err := stub.InsertRow("CompanyRegistration", shim.Row{
 		Columns: []*shim.Column{
 			&shim.Column{Value: &shim.Column_String_{String_: "CompanyRegistration"}},
 			&shim.Column{Value: &shim.Column_String_{String_: CompanyId}},
 			&shim.Column{Value: &shim.Column_String_{String_: CompanyName}},
 			&shim.Column{Value: &shim.Column_String_{String_: CompanyStatus}},
-			&shim.Column{Value: &shim.Column_String_{String_: CompanyRegTime}}
-
+			&shim.Column{Value: &shim.Column_String_{String_: CompanyRegTime}}},
 	})
 
 	if !ok && err == nil {
@@ -79,13 +69,11 @@ func (t *Company) Init(stub shim.ChaincodeStubInterface, function string, args [
 	return nil, err
 }
 
+func (t *Company) GetCD(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 
-func (t *Company) GetCD (stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-
-		if len(args) != 1 {
+	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1.")
-		}
-
+	}
 
 	CompanyId := args[0]
 
@@ -105,19 +93,18 @@ func (t *Company) GetCD (stub shim.ChaincodeStubInterface, args []string) ([]byt
 
 	// GetRows returns empty message if key does not exist
 	if len(row.Columns) == 0 {
-		
-	companyJSON.CompanyId = ""
-	companyJSON.CompanyName = ""
-	companyJSON.CompanyStatus = ""
-	companyJSON.CompanyRegTime = ""
+
+		companyJSON.CompanyId = ""
+		companyJSON.CompanyName = ""
+		companyJSON.CompanyStatus = ""
+		companyJSON.CompanyRegTime = ""
 
 	} else {
 
-
-	companyJSON.CompanyId = row.Columns[2].GetString_()
-	companyJSON.CompanyName = row.Columns[3].GetString_()
-	companyJSON.CompanyStatus = row.Columns[4].GetString_()
-	companyJSON.CompanyRegTime = row.Columns[5].GetString_())
+		companyJSON.CompanyId = row.Columns[2].GetString_()
+		companyJSON.CompanyName = row.Columns[3].GetString_()
+		companyJSON.CompanyStatus = row.Columns[4].GetString_()
+		companyJSON.CompanyRegTime = row.Columns[5].GetString_()
 
 	}
 
@@ -130,7 +117,6 @@ func (t *Company) GetCD (stub shim.ChaincodeStubInterface, args []string) ([]byt
 
 	fmt.Println(jsonCR)
 
- 	return jsonCR, nil
+	return jsonCR, nil
 
-	}
-
+}
